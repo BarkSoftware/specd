2fa3f848699b60519d8108872f1aeedf0e213f7d
Brandon Joyce <bjoyce@covermymeds.com>
2015-11-18 20:25:04 -0500

    Removing seeds, tokens are invalidated already

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.create!(
-  email: 'brandon@sonerdy.com',
-  provider: 'github',
-  uid: '46870',
-  token: '5b87cf14804e55b0d0b252fa5e5b877d3860732c',
-  full_name: 'Brandon Joyce',
-  image: 'https://avatars.githubusercontent.com/u/46870?v=3',
-  nickname: 'BrandonJoyce356',
-)
-
-User.create!(
-  email: 'brandonjoyce356@hotmail.com',
-  provider: 'github',
-  uid: '4210762',
-  token: 'f543ee6d25529089bf307e531109b16beba8b2c5',
-  full_name: 'EvilWhiteHot',
-  image: 'https://avatars.githubusercontent.com/u/4210762?v=3',
-  nickname: 'EvilWhiteHot',
-)
