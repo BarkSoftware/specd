db631222a7f13ba45af529de89b004d151083574
Brandon Joyce <bjoyce@covermymeds.com>
2015-12-21 20:14:09 -0500

    dedupe issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,9 +1,21 @@
 class Issue < ActiveRecord::Base
   belongs_to :column
   before_create :default_issue_type
+  validates_uniqueness_of :number, :scope => :project_id
 
   delegate :project, to: :column
   attr_accessor :github_issue
+
+  def self.dedupe
+    grouped = all.group_by{|model| [model.project_id, model.number] }
+    grouped.values.each do |duplicates|
+      # the first one we want to keep right?
+      first_one = duplicates.shift # or pop for last one
+      # if there are any more left, they are duplicates
+      # so delete all of them
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
 
   def move_to column
     self.column = column
